37eef5f15f67a6854cc08a45fc86270d587bfba7
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-05-25 16:08:08 +0200

    Added tableviewcells with individual files

---
+++ Surveys/TableViewCells/TelevisionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TelevisionTableViewCell.swift
+//  Surveys
+//
+//  Created by Nizaam Haffejee on 2024/05/24.
+//
+
+import UIKit
+
+class TelevisionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
