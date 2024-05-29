2c3fc7a17b4ec7b74711620198400e23182e0fb3
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-05-29 02:00:46 +0200

    Changed decimal point value

--- Surveys/Views/SurveyResultsViewController.swift
+++ Surveys/Views/SurveyResultsViewController.swift
@@ -46,7 +46,7 @@ class SurveyResultsViewController: UIViewController {
             likesPapAndWors.text = "%\(papAndWorsPercentage())"
             
             likesMovies.text = "Avg: \(String(format: "%.1f", movieLikeAverage())) (\(movieLikePercentage)%)"
-            likesRadio.text = "Avg: \(String(format: "%.2f", radioLikeAverage())) (\(radioLikePercentage)%)"
+            likesRadio.text = "Avg: \(String(format: "%.1f", radioLikeAverage())) (\(radioLikePercentage)%)"
             likesEatOut.text = "Avg: \(String(format: "%.2f", eatOutLikeAverage())) (\(eatOutLikePercentage)%)"
             likesTelevision.text = "Avg: \(String(format: "%.2f", televisionLikeAverage())) (\(televisionLikePercentage)%)"
         }
